2ce55e4a7f97bf7e1b258c878168811d28cdb7f1
Housey <housebreaker1@gmail.com>
2016-08-26 16:25:37 +0300

    refactor identity models

--- BIRA Issue Tracker/Models/Identity/IdentityModels.cs
+++ BIRA Issue Tracker/Models/Identity/IdentityModels.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace BIRA_Issue_Tracker.Models
+namespace BIRA_Issue_Tracker.Models.Identity
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
